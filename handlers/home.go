9b76c498153c8fb77b3d34db081be855eb69f2bd
somotto <stephenonyango4783@gmail.com>
2024-07-03 16:00:07 +0300

    feat: init home page handler

---
+++ handlers/home.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+        http.NotFound(w, r)
+        return
+    }
+
+    tmpl, err := template.ParseFiles("templates/index.html")
+    if err != nil {
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
+
+    tmpl.Execute(w, nil)
+}
